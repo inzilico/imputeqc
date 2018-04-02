8c7bca4c060c323bde40896604ca72981a33ec11
inzilico <info@inzilico.com>
2018-04-02 09:34:16 +0300

    Script to convert HapMap to fastPHASE is added.

--- inst/extdata/hapmap2plink.R
+++ inst/extdata/hapmap2plink.R
@@ -1,14 +1,19 @@
-## Convert gziped HapMap file into standard text Plink ped file accompanied with
-## map file. The output are filename.ped and filename.map, where `filename` is a
-## basename extracted from input filename.txt.gz file.
-## Usage: Rscript hapmap2plink.R filename.txt.gz
+## Convert gziped HapMap file into standard text Plink ped/map files. The output
+## are filename.ped and filename.map, where `filename` is a basename extracted
+## from input filename.txt.gz file. 'filename' can be provided as a second
+## argument.
+## Usage: Rscript hapmap2plink.R <filename.txt.gz> [output]
 ## Author: Gennady Khvorykh, inzilico.com
 
-isBiallelic <- function(x) {
+isBi <- function(x) {
   tmp <- unlist(strsplit(x, split = ""))
   tmp <- tmp[tmp != 0]
   length(unique(tmp)) <= 2
 }
+
+# ## TODO: Remove after debug
+# args <- c("~/data/hapmap/phaseI/genotypes_chr22_CEU.txt.gz",
+#           "~/data/hapmap/phaseI/unrelated.txt")
 
 args <- commandArgs(TRUE)
 
@@ -18,9 +23,11 @@ if(is.na(args[1]))
 
 if(!file.exists(args[1])) stop(args[1], " doesn't exist!")
 
-# Get basename
-fn <- basename(args[1])
-fn <- sub(".txt.gz", "", fn)
+# Set basename for output
+if(is.na(args[2])) {
+  fn <- basename(args[1])
+  fn <- sub(".txt.gz", "", fn)
+} else { fn <- args[2]}
 
 # Show input
 message("HapMap file: ", args[1])
@@ -37,7 +44,7 @@ data <- do.call(rbind, data)
 # Create map data
 map <- data[-1, c(3, 1, 4, 5)]
 
-# Leave only observations with strand +
+# Leave only observations at strand +
 ind <- map[, 4] == "+"
 map <- map[ind, ]
 
@@ -55,21 +62,20 @@ message("Individuals: ", length(ids))
 
 ped <- ped[-1, ]
 ped <- ped[ind, ]
-ped <- t(ped)
 
 # Replace any letter except A, T, C, G by 0 (missing allele)
 ped <- apply(ped, 2, gsub, pattern = "[^ATCG]", replacement = "0")
 
 # Leave only biallelic sites
-biallelic <- apply(ped, 2, isBiallelic)
-map <- map[biallelic, ]
-ped <- ped[, biallelic]
+bi <- apply(ped, 1, isBi)
+map <- map[bi, ]
+ped <- ped[bi, ]
 
-if (sum(!biallelic) > 0) message("Non biallelic: ", sum(!biallelic))
+if (sum(!bi) > 0) message("Non biallelic: ", sum(!bi))
 message("SNPs: ", nrow(map))
 
 #  Accomplish the creation of ped
-ped <- cbind(0, ids, 0, 0, 0, -9, ped)
+ped <- cbind(0, ids, 0, 0, 0, -9, t(ped))
 
 # Save ped/map
 write.table(map[, c(1, 2, 5, 3)], paste0(fn, ".map"), col.names = F, row.names = F, quote = F)
